8f5de7860c9dd9146ff9fd14625e110f71fb07c5
Carlos <carlosdaniel.londono@utp.edu.co>
2022-09-27 23:56:49 -0500

    Final version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # API Covid-19 Project
-Project that consumes data from Covid-19 cases in Colombia through the API (Sodapi) that Datos Abiertos Colombia provides us.
+Project that gets data from Covid-19 cases in Colombia through the API (Sodapi) that Datos Abiertos Colombia provides us.
 
 Requirements
 - Install libraries with the next comands
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from api import datos_abiertos
-from ui import interface, filter_information
+from data import api
+from ui import interface, filter_data
 
 
 if __name__ == '__main__':
     registers_limit, departament_name = interface.menu()
-    data = datos_abiertos.get_data(registers_limit, departament_name)
-    filtered_data = filter_information.filter_columns(data)
+    data = api.get_data(registers_limit, departament_name)
+    filtered_data = filter_data.filter_columns(data)
     print(filtered_data)
